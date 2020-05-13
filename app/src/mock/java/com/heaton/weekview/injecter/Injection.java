e045ca3ea71ae0c40daa95e58ed4398d1fd20e3e
heatonlin <heatonlin@emotibot.com>
2020-05-13 19:12:46 +0800

    update Injection

---
+++ app/src/mock/java/com/heaton/weekview/injecter/Injection.java
@@ -0,0 +1,21 @@
+package com.heaton.weekview.injecter;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.heaton.weekview.model.ClassDataSource;
+import com.heaton.weekview.model.ClassRepository;
+import com.heaton.weekview.model.MockLocalClassDataSource;
+import com.heaton.weekview.model.remoteDataSource.RemoteClassDataSource;
+
+import static com.google.gson.internal.$Gson$Preconditions.checkNotNull;
+
+public class Injection {
+
+    public static ClassDataSource provideDataSource(@NonNull Context context) {
+        checkNotNull(context);
+        return ClassRepository.getInstance(MockLocalClassDataSource.getInstance(context)
+                , RemoteClassDataSource.getInstance());
+    }
+}
